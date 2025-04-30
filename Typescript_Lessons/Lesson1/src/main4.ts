cb08c905ef666ce864a02301ab01d15e76af0895
nyore192 <nickyore@yahoo.com>
2025-04-30 11:34:49 -0700

    lesson4
    lesson4

---
+++ Typescript_Lessons/Lesson1/src/main4.ts
@@ -0,0 +1,65 @@
+// Type Alliases
+type stringOrNumber = string | number;
+
+type stringOrNumberArray = (string | number)[];
+
+interface Guitarist {
+  name?: string;
+  active: boolean;
+  albums: (string | number)[];
+}
+
+type UserId = stringOrNumber;
+
+// Literal types
+let myNameTwo: "Nick";
+
+let userName: "Nick" | "Jon" | "Amy";
+userName = "Nick";
+
+//Functions
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+const logMsg = (message: any): void => {
+  console.log(message);
+};
+
+logMsg("Hello!");
+logMsg(add(2, 3));
+
+let subtract = function (c: number, d: number): number {
+  return c - d;
+};
+
+type mathFunction = (a: number, b: number) => number;
+
+//interface mathFunction { (a: number, b: number):  number
+//}
+
+let multiply: mathFunction = function (c, d) {
+  return c * d;
+};
+
+logMsg(multiply(2, 2));
+
+// optional parameters
+const addAll = (a: number, b: number, c?: number): number => {
+  if (typeof c !== "undefined") {
+    return a + b + c;
+  }
+  return a + b;
+};
+
+// default param value
+const sumAll = (a: number = 10, b: number, c: number = 2): number => {
+  return a + b + c;
+};
+
+logMsg(addAll(2, 3, 2));
+logMsg(addAll(2, 3));
+logMsg(sumAll(2, 3, 5));
+
+
+// Rest Parameters
